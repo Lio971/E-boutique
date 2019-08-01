45663bf450c99b2cfff29ea14d5c62bcae0eed95
Rémi <remilp@hotmail.com>
2019-08-02 01:02:45 +0200

    link city to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@ User.destroy_all
     last_name:FAKER::Name.last_name, 
     description:Faker::Quote.matz, 
     email:Faker::Internet.email, 
-    age:rand(14..34))
+    age:rand(14..34),
+    city:City.find(rand(City.first.id..City.last.id)))
+end
+
+Tu vas faire 10 villes en base avec Faker et les lier avec les utilisateurs du seed.
+City.destroy_all
+10.times do
+    City.create!(name:FAKER::Adress.city, 
+    zip_code: Faker::Address.zip_code)
 end
