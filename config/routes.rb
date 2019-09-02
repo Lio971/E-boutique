08471fa009b4819f51c0f7c27d800f05a4bab223
Elkatrani Ismaïl <ielkatrani@gmail.com>
2019-09-02 15:51:46 +0200

    we can add a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
