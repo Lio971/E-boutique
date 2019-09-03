d3dd09949acbcb0f261b8cb692d7e2915b5bacb5
Rémi <remilp@hotmail.com>
2019-09-03 11:48:40 +0200

    20 seeds en base

---
+++ EcomMardi/https:/github.com/remilp89/E-boutique/db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Item.destroy_all
+20.times do
+Item.create!(
+title: Faker::Creature::Cat.name,
+description: Faker::Hipster.sentence,
+price: rand(10..50),
+image_url: Faker::Avatar.image,
+)
